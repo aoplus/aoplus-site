058926db8cbe83e3f5b7d84cb8da80c4bc39ec5f
AO PLUS <aoplusin@gmail.com>
2025-09-11 11:08:57 +0000

    please deploy in github pages

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type {NextConfig} from 'next';
 
+const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: repoName ? `/${repoName}` : '',
+  assetPrefix: repoName ? `/${repoName}/` : '',
   typescript: {
     ignoreBuildErrors: true,
   },
